f7d54eb00cc5054649a3c8d03fb81ef2a1550dd6
VWMAC <vincentkywong@gmail.com>
2019-09-14 21:58:15 -0700

    create own component to import

--- 3.26/src/App.js
+++ 3.26/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div className="App">
         <h1> Hi, I'm a react App </h1>
         <p>This is working</p>
+        <Person />
       </div>
       // <h1>Another heading</h1>
     );
