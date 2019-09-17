c9473c538d2139d12f4b0e5795749309ed9e4f28
VWMAC <vincentkywong@gmail.com>
2019-09-16 23:48:02 -0700

    methods - can pass methods as props to other compos

--- 3.26/src/App.js
+++ 3.26/src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   state = {
     persons: [
-      { name: 'max', age: 28 },
-      { name: 'manu', age: 29 },
-      { name: 'steph', age: 26 },
+      { name: "max", age: 28 },
+      { name: "manu", age: 29 },
+      { name: "steph", age: 26 }
     ]
-  }
+  };
 
   switchNameHandler = () => {
     // console.log('was clicked');
     // this.state.persons[0].name = 'maxy';
     this.setState({
       persons: [
-        { name: 'maxie', age: 28 },
-        { name: 'manu', age: 29 },
-        { name: 'steph', age: 27 },
+        { name: "maxie", age: 28 },
+        { name: "manu", age: 29 },
+        { name: "steph", age: 27 }
       ],
-      otherState: 'some other value'
-    })
-  }
+      otherState: "some other value"
+    });
+  };
 
   render() {
     return (
@@ -30,9 +30,21 @@ class App extends Component {
         <h1> Hi, I'm a react App </h1>
         <p>This is working</p>
         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler}
+        >
+          My hobbies: racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
       // <h1>Another heading</h1>
     );
