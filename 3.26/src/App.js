ba61063d56af16c4b0949568413007bbf097a5cd
VWMAC <vincentkywong@gmail.com>
2019-09-19 02:48:48 -0700

    pass argv with anon fnc

--- 3.26/src/App.js
+++ 3.26/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
       <div className="App">
         <h1> Hi, I'm a react App </h1>
         <p>This is working</p>
-        <button onClick={this.switchNameHandler.bind(this, 'maximilion')}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('maximilioan!!')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -45,7 +45,7 @@ class App extends Component {
           name={this.state.persons[2].name}
           age={this.state.persons[2].age}
         />
-      </div>
+      </div >
       // <h1>Another heading</h1>
     );
     //return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now'))
