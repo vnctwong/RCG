2302a527f4c7a44588ae34322fa3dd5f7265745a
VWMAC <vincentkywong@gmail.com>
2019-09-21 14:30:04 -0700

    outputting lists - can use this.state.map(el => { return <jsx data ={el.data} />)}

--- 3.26/src/App.js
+++ 3.26/src/App.js
@@ -55,24 +55,13 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'max!')}
-            changed={this.nameChangedHandler}
-          >
-            My hobbies: racing
-        </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map(person => {
+            return <Person
+              name={person.name}
+              age={person.age} />
+          })}
         </div>
-      )
+      );
     }
 
     return (
