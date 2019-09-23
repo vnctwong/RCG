bfbdf3f2c7efa72449a29340d03d491635f41c3c
VWMAC <vincentkywong@gmail.com>
2019-09-22 23:22:48 -0700

    altering lists - target index of state array - use js methods splice, map etc

--- 3.26/src/App.js
+++ 3.26/src/App.js
@@ -13,18 +13,6 @@ class App extends Component {
     showPersons: false
   };
 
-  switchNameHandler = (newName) => {
-    // console.log('was clicked');
-    // this.state.persons[0].name = 'maxy';
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: "manu", age: 29 },
-        { name: "stephanie", age: 27 }
-      ]
-    })
-  };
-
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
@@ -33,6 +21,12 @@ class App extends Component {
         { name: "stephanie", age: 26 }
       ]
     })
+  };
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons })
   };
 
   togglePersonHandler = () => {
@@ -55,8 +49,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person, index) => {
             return <Person
+              click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age} />
           })}
