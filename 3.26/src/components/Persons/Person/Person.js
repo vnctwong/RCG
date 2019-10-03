2445f8f0de34c3d440406a5d521382c874bce7b8
VWMAC <vincentkywong@gmail.com>
2019-10-02 17:36:55 -0700

    restructure project folders

--- 3.26/src/components/Persons/Person/Person.js
+++ 3.26/src/components/Persons/Person/Person.js
@@ -7,7 +7,9 @@ const person = (props) => {
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old
       </p>
+
       <p>{props.children}</p>
+
       <input type='text' onChange={props.changed} value={props.name} />
     </div>
   );
