3ea3de542965d829ba82767ae7f192da36ca1c66
VWMAC <vincentkywong@gmail.com>
2019-10-04 23:39:13 -0700

    useEffect - always a function - replaces lifecycle hooks did didMount didUpdate etc

--- 3.26/src/components/Cockpit/Cockpit.js
+++ 3.26/src/components/Cockpit/Cockpit.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    // i.e http request...
+  });
+
   const assignedClasses = [];
   let btnClass = '';
 
