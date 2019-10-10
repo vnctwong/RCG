8f6f3c7561019ba6305a5b169defc94c13474c2a
VWMAC <vincentkywong@gmail.com>
2019-10-09 23:50:39 -0700

    HOC - component that wraps other component that adds additional func - can add class by passing props.classes to props.children

--- 3.26/src/containers/App.js
+++ 3.26/src/containers/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+
 import classes from "./App.css";
-import Persons from "../components/Persons/Persons";
 import Cockpit from '../components/Cockpit/Cockpit';
+import Persons from "../components/Persons/Persons";
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
   constructor(props) {
@@ -77,7 +79,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button
           onClick={() => {
             this.setState({ showCockpit: false });
@@ -94,7 +96,7 @@ class App extends Component {
           />
         ) : null}
         {persons}
-      </div >
+      </WithClass >
     );
     //return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now'))
   }
