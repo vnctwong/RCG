da4d01c5933a48a76abfe8d4207d87340d791f1a
VWMAC <vincentkywong@gmail.com>
2019-10-10 23:55:04 -0700

    setting state for class compo - for states depend on old state, setState()=>, pass prevState as props

--- 3.26/src/containers/App.js
+++ 3.26/src/containers/App.js
@@ -20,7 +20,8 @@ class App extends Component {
     ],
     otherState: 'some other state',
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -52,7 +53,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      };
+    });
   };
 
   deletePersonHandler = (personIndex) => {
