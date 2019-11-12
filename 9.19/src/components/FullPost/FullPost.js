fae45709df1c9f0db72bd491659dd63e07514e40
VWMAC <vincentkywong@gmail.com>
2019-11-12 00:15:55 -0800

    fullPost text align center

--- 9.19/src/components/FullPost/FullPost.js
+++ 9.19/src/components/FullPost/FullPost.js
@@ -4,7 +4,7 @@ import './FullPost.css';
 
 class FullPost extends Component {
   render() {
-    let post = <p>Please select a Post!</p>;
+    let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
 
     if (this.props.id) {
       post = (
