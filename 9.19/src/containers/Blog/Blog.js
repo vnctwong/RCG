9b0456e05055edffb29777b6b694bedf5413c54a
VWMAC <vincentkywong@gmail.com>
2019-11-11 23:50:00 -0800

    transforming and adding attributes to json data

--- 9.19/src/containers/Blog/Blog.js
+++ 9.19/src/containers/Blog/Blog.js
@@ -14,16 +14,22 @@ class Blog extends Component {
   componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(res => {
-        this.setState({ posts: res.data });
+        const posts = res.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'max'
+          }
+        });
+        this.setState({ posts: updatedPosts });
         // console.log(res)
       });
   }
 
   render() {
     const posts = this.state.posts.map(post => {
-      return <Post key={post.id} title={post.title} />
-    }
-    )
+      return <Post key={post.id} title={post.title} author={post.author} />
+    });
 
     return (
       <div>
