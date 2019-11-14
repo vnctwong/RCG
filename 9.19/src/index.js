a8387ec34fd293d6f48a51f637d2579a99e709f1
VWMAC <vincentkywong@gmail.com>
2019-11-13 23:58:06 -0800

    refractor index

--- 9.19/src/index.js
+++ 9.19/src/index.js
@@ -7,20 +7,20 @@ import axios from 'axios';
 
 axios.interceptors.request.use(
   request => {
-    console.log(request);
+    // console.log(request);
     return request;
   }, error => {
-    console.log(error);
+    // console.log(error);
     return Promise.reject(error);
   }
 );
 
 axios.interceptors.response.use(
   response => {
-    console.log(response);
+    // console.log(response);
     return response;
   }, error => {
-    console.log(error);
+    // console.log(error);
     return Promise.reject(error);
   }
 );
