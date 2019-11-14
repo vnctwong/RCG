8d3fb25e3aef337262cea3c7fae7d9b09dd8d297
VWMAC <vincentkywong@gmail.com>
2019-11-13 22:23:33 -0800

    delete button on fullPost works

--- 9.19/src/components/FullPost/FullPost.js
+++ 9.19/src/components/FullPost/FullPost.js
@@ -11,13 +11,20 @@ class FullPost extends Component {
   componentDidUpdate() {
     if (this.props.id) {
       if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-        axios.get('https://jsonplaceholder.typicode.com/posts' + this.props.id)
+        axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
           .then(response => {
             console.log(response);
             this.setState({ loadedPost: response.data });
           });
       }
     }
+  }
+
+  deletePostHandler = () => {
+    axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+      .then(response => {
+        console.log(response)
+      })
   }
 
   render() {
@@ -31,7 +38,7 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
-            <button className="Delete">Delete</button>
+            <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
           </div>
         </div>
 
