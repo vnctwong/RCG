f05c37cf30b1914d279f53c598d5d2b7ea535b8d
VWMAC <vincentkywong@gmail.com>
2019-11-19 23:52:42 -0800

    restructure all modules into containers

--- 9.19/src/containers/Blog/Blog.js
+++ 9.19/src/containers/Blog/Blog.js
@@ -1,56 +1,13 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import axios from '../../axios';
-
-import Post from '../../components/Post/Post';
-import FullPost from './FullPost/FullPost';
-import NewPost from './NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false
-  }
-
-  componentDidMount() {
-    axios.get('/posts')
-      .then(response => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map(post => {
-          return {
-            ...post,
-            author: 'Max'
-          }
-        });
-        this.setState({ posts: updatedPosts });
-        // console.log(response);
-      })
-      .catch(error => {
-        // console.log(error);
-        this.setState({ error: true })
-      });
-  }
-
-  postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
-  }
 
   render() {
-    let posts = <p style={{ textAlign: 'center' }}>Something went wrong</p>
-    if (!this.state.error) {
-      posts = this.state.posts.map(post => {
-        return <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)} />;
-      });
-    }
-
     return (
-      <div className='Blog'>
+      <div className="Blog" >
         <header>
           <nav>
             <ul>
@@ -60,9 +17,7 @@ class Blog extends Component {
           </nav>
         </header>
 
-        <section className="Posts">
-          {posts}
-        </section>
+        <Posts />
       </div>
     );
   }
