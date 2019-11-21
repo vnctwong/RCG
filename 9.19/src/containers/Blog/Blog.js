6c7e186cf8427907dea34a54db9c1dbb65f478da
VWMAC <vincentkywong@gmail.com>
2019-11-20 23:59:10 -0800

    use Navlink to style conditional on router.props

--- 9.19/src/containers/Blog/Blog.js
+++ 9.19/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
 
 
@@ -16,12 +16,12 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li> <Link to='/'>Home </Link></li>
-              <li><Link to={{
+              <li> <NavLink to='/' exact>Home </NavLink></li>
+              <li><NavLink to={{
                 pathname: this.props.match.url + '/new-post',
                 hash: '#submit',
                 search: '?quick-submit=true'
-              }}>New Post</Link></li>
+              }}>New Post</NavLink></li>
             </ul>
           </nav>
         </header>
