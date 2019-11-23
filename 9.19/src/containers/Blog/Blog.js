30f6bb0930bd62ced49de63e676a3147740f41c4
VWMAC <vincentkywong@gmail.com>
2019-11-22 23:05:02 -0800

    blog uses switch compo to only load single route

--- 9.19/src/containers/Blog/Blog.js
+++ 9.19/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -24,10 +24,11 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-
-        <Route path='/' exact component={Posts} />
-        <Route path='/new-post' exact component={NewPost} />
-        <Route path='/:id' exact component={FullPost} />
+        <Switch>
+          <Route path='/' exact component={Posts} />
+          <Route path='/new-post' exact component={NewPost} />
+          <Route path='/:id' exact component={FullPost} />
+        </Switch>
       </div>
     );
   }
