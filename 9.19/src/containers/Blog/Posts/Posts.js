8e320e7b4454aa17a5ed7a66c4d1ffd9a8c508ba
VWMAC <vincentkywong@gmail.com>
2019-11-22 23:14:09 -0800

    using router props attr to redirect

--- 9.19/src/containers/Blog/Posts/Posts.js
+++ 9.19/src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
-import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
@@ -32,7 +31,7 @@ class Posts extends Component {
   }
 
   postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
+    this.props.history.push('/' + id)
   }
 
   render() {
@@ -40,12 +39,11 @@ class Posts extends Component {
     if (!this.state.error) {
       posts = this.state.posts.map(post => {
         return (
-          <Link to={'/' + post.id} key={post.id}>
-            <Post
-              title={post.title}
-              author={post.author}
-              clicked={() => this.postSelectedHandler(post.id)} />
-          </Link>
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)} />
         );
       });
     }
