40dc86cabbda4d08fe46a280b71498fa1b196593
VWMAC <vincentkywong@gmail.com>
2019-11-22 23:47:16 -0800

    switch nested route to render both post/fullposts

--- 9.19/src/containers/Blog/Blog.js
+++ 9.19/src/containers/Blog/Blog.js
@@ -15,7 +15,7 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li> <NavLink to='/' exact>Home </NavLink></li>
+              <li> <NavLink to='/' exact>Posts</NavLink></li>
               <li><NavLink to={{
                 pathname: '/new-post',
                 hash: '#submit',
@@ -25,9 +25,8 @@ class Blog extends Component {
           </nav>
         </header>
         <Switch>
-          <Route path='/' exact component={Posts} />
           <Route path='/new-post' exact component={NewPost} />
-          <Route path='/:id' exact component={FullPost} />
+          <Route path='/' exact component={Posts} />
         </Switch>
       </div>
     );
