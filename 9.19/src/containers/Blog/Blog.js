6365380f1ca3c92c8489eee0e52848f662ecebbe
VWMAC <vincentkywong@gmail.com>
2019-11-24 23:12:35 -0800

    refrac to use consistent routing

--- 9.19/src/containers/Blog/Blog.js
+++ 9.19/src/containers/Blog/Blog.js
@@ -4,33 +4,40 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from '../../containers/Blog/NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
-
-  render() {
-    return (
-      <div className="Blog" >
-        <header>
-          <nav>
-            <ul>
-              <li> <NavLink to='/posts/' exact>Posts</NavLink></li>
-              <li><NavLink to={{
-                pathname: '/new-post',
-                hash: '#submit',
-                search: '?quick-submit=true'
-              }}>New Post</NavLink></li>
-            </ul>
-          </nav>
-        </header>
-        <Switch>
-          <Route path='/new-post' exact component={NewPost} />
-          <Route path='/posts' exact component={Posts} />
-        </Switch>
-      </div>
-    );
-  }
+    render () {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                <Switch>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default Blog;
--- 9.19/src/index.js
+++ 9.19/src/index.js
@@ -9,25 +9,23 @@ axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-axios.interceptors.request.use(
-  request => {
-    // console.log(request);
+axios.interceptors.request.use(request => {
+    console.log(request);
+    // Edit request config
     return request;
-  }, error => {
-    // console.log(error);
+}, error => {
+    console.log(error);
     return Promise.reject(error);
-  }
-);
+});
 
-axios.interceptors.response.use(
-  response => {
-    // console.log(response);
+axios.interceptors.response.use(response => {
+    console.log(response);
+    // Edit request config
     return response;
-  }, error => {
-    // console.log(error);
+}, error => {
+    console.log(error);
     return Promise.reject(error);
-  }
-);
+});
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
