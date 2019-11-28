e2880db378c8da060445c5952887b0c1a9ba727e
VWMAC <vincentkywong@gmail.com>
2019-11-27 23:41:38 -0800

    using suspense to conditional load

--- 11.246/src/App.js
+++ 11.246/src/App.js
@@ -7,22 +7,33 @@ import Welcome from './containers/Welcome';
 const Posts = React.lazy(() => import('./containers/Posts'));
 
 class App extends Component {
+  state = { showPosts: false };
+
+  modeHandler = () => {
+    this.setState(prevState => {
+      return { showPosts: !prevState.showPosts };
+    })
+  }
+
   render() {
     return (
-      <BrowserRouter>
-        <React.Fragment>
-          <nav>
-            <NavLink to="/user">User Page</NavLink> |&nbsp;
-            <NavLink to="/posts">Posts Page</NavLink>
-          </nav>
-          <Route path="/" component={Welcome} exact />
-          <Route path="/user" component={User} />
-          <Route path="/posts" render={() =>
-            <Suspense fallback={<div>Loading...</div>}>
-              <Posts />
-            </Suspense>} />
-        </React.Fragment>
-      </BrowserRouter>
+      <React.Fragment>
+        <button onClick={this.modeHandler} >Toggle Mode</button>
+        {this.state.showPosts ? (<Suspense fallback={<div>Loading...</div>}><Posts /></Suspense>) : <User />}
+      </React.Fragment>
+
+      // <BrowserRouter>
+      //   <React.Fragment>
+      //     <nav>
+      //       <NavLink to="/user">User Page</NavLink> |&nbsp;
+      //       <NavLink to="/posts">Posts Page</NavLink>
+      //     </nav>
+      //     <Route path="/" component={Welcome} exact />
+      //     <Route path="/user" component={User} />
+      //     <Route path="/posts" render={() =>
+
+      //   </React.Fragment>
+      // </BrowserRouter>
     );
   }
 }
