4590c0cc6ab78a11b38f11cc84b53a80120584d0
VWMAC <vincentkywong@gmail.com>
2019-12-12 17:06:08 -0800

    adding dispatch action

--- 14.285/redux-basics.js
+++ 14.285/redux-basics.js
@@ -7,12 +7,28 @@ const intialState = {
 
 // Reducer
 const rootReducer = (state = intialState, action) => {
+  if (action.type === 'INC_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === 'ADD_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
   return state;
-}
+};
+
 // Store
 const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 })
+console.log(store.getState());
 
 // Subscription
