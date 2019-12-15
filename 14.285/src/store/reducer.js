cdd6d60e8e6a3358437faf74e51c65fa68206068
VWMAC <vincentkywong@gmail.com>
2019-12-14 23:55:53 -0800

    refrac reducer to switchCase

--- 14.285/src/store/reducer.js
+++ 14.285/src/store/reducer.js
@@ -3,25 +3,24 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  if (action.type === 'INCREMENT') {
-    return {
-      counter: state.counter + 1
-    }
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        counter: state.counter + 1
+      }
+    case 'DECREMENT':
+      return {
+        counter: state.counter + 1
+      }
+    case 'ADD':
+      return {
+        counter: state.counter + action.value
+      }
+    case 'SUBJECT':
+      return {
+        counter: state.counter + action.value
+      }
   }
-  if (action.type === 'DECREMENT') {
-    return {
-      counter: state.counter - 1
-    }
-  } if (action.type === 'ADD') {
-    return {
-      counter: state.counter + action.value
-    }
-  } if (action.type === 'SUBTRACT') {
-    return {
-      counter: state.counter - action.value
-    }
-  }
-
   return state;
 };
 
