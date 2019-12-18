e70e37ecf3863386dbb585e856fd783c6ff779c1
VWMAC <vincentkywong@gmail.com>
2019-12-17 23:43:44 -0800

    updating arrays immutably to delete state

--- 14.285/src/containers/Counter/Counter.js
+++ 14.285/src/containers/Counter/Counter.js
@@ -17,7 +17,7 @@ class Counter extends Component {
         <button onClick={this.props.onStoreResult}>Store Result</button>
         <ul>
           {this.props.storedResults.map(strResult => (
-            <li key={strResult.id} onClick={this.props.onDeleteResult}> {strResult.value} </li>
+            <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}> {strResult.value} </li>
           ))}
         </ul>
       </div>
@@ -39,7 +39,7 @@ const mapDispatchToProps = dispatch => {
     onAddCounter: () => dispatch({ type: 'ADD', value: 10 }),
     onSubtractCounter: () => dispatch({ type: 'SUBTRACT', value: 10 }),
     onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
-    onDeleteResult: () => dispatch({ type: 'DELETE_RESULT' })
+    onDeleteResult: (id) => dispatch({ type: 'DELETE_RESULT', resultElId: id })
   }
 }
 
--- 14.285/src/store/reducer.js
+++ 14.285/src/store/reducer.js
@@ -29,6 +29,16 @@ const reducer = (state = initialState, action) => {
         ...state,
         results: state.results.concat({ id: new Date(), value: state.counter })
       }
+    case 'DELETE_RESULT':
+      // const id = 2;
+      // const newArray = [...state.results];
+      // newArray.splice(id, 1)
+      // if state.results has objs, newArray.results will still point to original array. Can delete, indices but not change them
+      const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+      return {
+        ...state,
+        results: updatedArray
+      }
   }
   return state;
 };
