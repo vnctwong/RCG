4ec63306d290b2b78e77b487261aa02e12897689
VWMAC <vincentkywong@gmail.com>
2020-01-26 16:45:37 -0800

    refrac result reducer to make dry

--- 14.285/src/store/reducers/result.js
+++ 14.285/src/store/reducers/result.js
@@ -5,17 +5,19 @@ const initialState = {
   results: []
 };
 
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+  return updatedObject(state, { results: updatedArray });
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+
     case actionTypes.STORE_RESULT:
       return updatedObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) });
+
     case actionTypes.DELETE_RESULT:
-      // const id = 2;
-      // const newArray = [...state.results];
-      // newArray.splice(id, 1)
-      // if state.results has objs, newArray.results will still point to original array. Can delete, indices but not change them
-      const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-      return updatedObject(state, { results: updatedArray });
+      return deleteResult(state, action);
   }
   return state;
 };
