7a4085e60f4f480b9b8741ff3aa557b0b318fcd7
VWMAC <vincentkywong@gmail.com>
2021-01-05 23:47:58 -0800

    new project init

--- 26.426/src/components/Ingredients/IngredientForm.js
+++ 26.426/src/components/Ingredients/IngredientForm.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const inputState = useState({ title: '', amount: '' });
-
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -15,39 +13,17 @@ const IngredientForm = React.memo(props => {
     <section className="ingredient-form">
       <Card>
         <form onSubmit={submitHandler}>
-
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input
-              type="text"
-              id="title"
-              value={inputState[0].title}
-              onChange={event =>
-                inputState[1]({
-                  title: event.target.value,
-                  amount: inputState[0].amount
-                })}
-            />
+            <input type="text" id="title" />
           </div>
-
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input
-              type="number"
-              id="amount"
-              value={inputState[0].amount}
-              onChange={event =>
-                inputState[1]({
-                  amount: event.target.value,
-                  title: inputState[0].title
-                })}
-            />
+            <input type="number" id="amount" />
           </div>
-
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
           </div>
-
         </form>
       </Card>
     </section>
