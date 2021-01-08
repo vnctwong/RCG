2d49426aa55f5e38d299e2ae7404ca000a8a01ca
VWMAC <vincentkywong@gmail.com>
2021-01-08 00:00:28 -0800

    form snapshot location change

--- 26.426/src/components/Ingredients/IngredientForm.js
+++ 26.426/src/components/Ingredients/IngredientForm.js
@@ -17,11 +17,24 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState[0].title} onChange={event => inputState[1]({ title: event.target.value })} />
+            <input
+              type="text" id="title"
+              value={inputState[0].title}
+              onChange={event =>
+                inputState[1](prevInputState => ({ title: event.target.value, amount: prevInputState[0].amount }))} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState[0].amount} onChange={event => inputState[1]({ amount: event.target.value })} />
+            <input
+              type="number" id="amount"
+              value={inputState[0].amount}
+              onChange={event =>
+                inputState[1](prevInputState => ({
+                  amount: event.target.value,
+                  title: prevInputState.title
+                }))
+              }
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
