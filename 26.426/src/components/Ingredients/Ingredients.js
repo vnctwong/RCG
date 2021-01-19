1948ef14ce84e4ef0399f1c8c80bb8fd0870df07
VWMAC <vincentkywong@gmail.com>
2021-01-18 23:59:25 -0800

    set up ingreident list

--- 26.426/src/components/Ingredients/Ingredients.js
+++ 26.426/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+
+  };
+
   return (
     <div className="App">
       <IngredientForm />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} />
       </section>
     </div>
   );
