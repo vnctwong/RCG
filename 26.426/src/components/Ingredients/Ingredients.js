108bbce00720885b7f59bde8cb358630e762c927
VWMAC <vincentkywong@gmail.com>
2021-01-19 23:44:09 -0800

    ingredientList displaying items

--- 26.426/src/components/Ingredients/Ingredients.js
+++ 26.426/src/components/Ingredients/Ingredients.js
@@ -8,16 +8,19 @@ const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
   const addIngredientHandler = ingredient => {
-
+    setUserIngredients(prevIngredients => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient }
+    ]);
   };
 
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} />
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => { }} />
       </section>
     </div>
   );
