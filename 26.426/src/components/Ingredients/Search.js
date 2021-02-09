5d4794dd4f6c5239561d6247ae26b20ee5f439b7
VWMAC <vincentkywong@gmail.com>
2021-02-08 23:58:57 -0800

    start logic for search bar

--- 26.426/src/components/Ingredients/Search.js
+++ 26.426/src/components/Ingredients/Search.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const [enteredfilter, setEnteredFilter] = useState('');
+
+  useEffect(() => {
+    fetch('https://hooks-prac-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        // onLoadingredients
+      });
+  }, [enteredfilter]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={enteredfilter}
+            onChange={event => enteredfilter(event.target.value)} />
         </div>
       </Card>
     </section>
