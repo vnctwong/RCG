c8f7ec7f34713536c6d725bd9d0f6fffc166f460
VWMAC <vincentkywong@gmail.com>
2021-03-06 23:53:48 -0800

    err on removeIngreident

--- 26.426/src/components/Ingredients/Ingredients.js
+++ 26.426/src/components/Ingredients/Ingredients.js
@@ -14,10 +14,8 @@ const Ingredients = () => {
     ]);
   };
 
-  const removeIngredientHandler = ingredientId => {
-    setUserIngredients(prevIngredients =>
-      prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
-    );
+  const removeIngredientHandler = ingredient => {
+
   };
 
   return (
@@ -26,10 +24,7 @@ const Ingredients = () => {
 
       <section>
         <Search />
-        <IngredientList
-          ingredients={userIngredients}
-          onRemoveItem={removeIngredientHandler}
-        />
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => { removeIngredientHandler }} />
       </section>
     </div>
   );
