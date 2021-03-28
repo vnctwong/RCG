712753758fc61801e9c5364c0955eca42878c940
VWMAC <vincentkywong@gmail.com>
2021-03-27 23:59:23 -0700

    start logic for search re-render

--- 26.426/src/components/Ingredients/Search.js
+++ 26.426/src/components/Ingredients/Search.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const [enteredFilter, setEnteredFilter] = useState('');
+
+  useEffect(() => {
+    fetch('https://rcg-26-426-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key, title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        // logic for passing keystrokes to <Search> in ingredients.js for new ingredientList render
+      });
+  }, [enteredFilter])
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={enteredFilter}
+            onChange={event => setEnteredFilter(event.target.value)}
+          />
         </div>
       </Card>
     </section>
