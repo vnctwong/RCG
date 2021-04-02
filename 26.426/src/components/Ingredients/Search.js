5d8e778f06dcb24a81ed81e49aa4129d8e037d64
VWMAC <vincentkywong@gmail.com>
2021-04-01 18:49:11 -0700

    useEffect clears old timer on keystroke

--- 26.426/src/components/Ingredients/Search.js
+++ 26.426/src/components/Ingredients/Search.js
@@ -9,7 +9,7 @@ const Search = React.memo(props => {
   const inputRef = useRef();
 
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       if (enteredFilter === inputRef.current.value) {
         const query =
           enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
@@ -27,6 +27,9 @@ const Search = React.memo(props => {
           });
       }
     }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
   }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
