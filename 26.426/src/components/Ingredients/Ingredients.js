284a652b2d5027eb95f9b4ea8712e34556317f6e
VWMAC <vincentkywong@gmail.com>
2021-04-09 23:56:20 -0700

    switch http state management to useReducer

--- 26.426/src/components/Ingredients/Ingredients.js
+++ 26.426/src/components/Ingredients/Ingredients.js
@@ -1,9 +1,21 @@
-import React, { useState, useCallback } from 'react';
+import React, { useReducer, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
+
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient]
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id);
+    default: throw new Error('This is error');
+  }
+}
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
