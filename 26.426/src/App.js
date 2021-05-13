8eeff31b2fb869077351dfcbafd412f52ce1d59d
VWMAC <vincentkywong@gmail.com>
2021-05-12 23:09:37 -0700

    create auth provider w/ useContext

--- 26.426/src/App.js
+++ 26.426/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Ingredients from './components/Ingredients/Ingredients';
+import Auth from './components/Auth';
 
 const App = props => {
   return <Ingredients />;
---
+++ 26.426/src/context/auth-context.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+export const AuthContext = React.createContext({
+  isAuth: false,
+  login: () => { }
+});
+
+const AuthContextProvider = props => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const loginHandler = () => {
+    setIsAuthenticated(true);
+  };
+
+  return (
+    <AuthContextProvider value={{ login: loginHandler, isAuth: isAuthenticated }} >
+      {props.children}
+    </AuthContextProvider>
+  );
+
+}
+
+export default AuthContextProvider;
