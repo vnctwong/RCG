ed9c01f55bcea49ba32eec02c9db647f36a786a5
VWMAC <vincentkywong@gmail.com>
2021-06-16 23:59:28 -0700

    reload missing files from git

---
+++ 3.28/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>day month year</div>
+      <div>
+        <h2>Car insurance</h2>
+        <div>$xx.xx</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
