b830b589c9636bc206ec5d8e2d2cdc6713f70d77
VWMAC <vincentkywong@gmail.com>
2021-06-22 23:52:44 -0700

    fix date type

--- 3.28/src/components/ExpenseItem.js
+++ 3.28/src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
-  const expenseDate = new Date(yy, mm, dd)
+  const expenseDate = new Date(2021, 6, 21)
   const expenseTitle = 'expense';
   const expenseAmount = '$xx.xx';
 
   return (
     <div className="expense-item">
-      <div>{expenseDate}</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item_description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">{expenseAmount}</div>
