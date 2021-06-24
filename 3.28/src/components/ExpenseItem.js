f0246a3fe6163bacca6ba08d7f8af7af70b10fea
VWMAC <vincentkywong@gmail.com>
2021-06-23 23:51:15 -0700

    move data to appJS

--- 3.28/src/components/ExpenseItem.js
+++ 3.28/src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 6, 21)
-  const expenseTitle = 'expense';
-  const expenseAmount = '$xx.xx';
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item_description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
