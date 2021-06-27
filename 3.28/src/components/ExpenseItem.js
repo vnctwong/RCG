59be91451571b74ccf9a137e6de907a54de66d67
VWMAC <vincentkywong@gmail.com>
2021-06-26 23:16:17 -0700

    fix date input

--- 3.28/src/components/ExpenseItem.js
+++ 3.28/src/components/ExpenseItem.js
@@ -2,18 +2,20 @@ import './ExpenseItem.css';
 
 function ExpenseItem(props) {
   const month = props.date.toLocaleString('en-us', { month: 'long' });
+  const day = props.date.toLocaleString('en-us', { day: '2-digit' });
+  const year = props.date.getFullYear();
   return (
-    <div className="expense-item">
+    <div className="expense-item" >
       <div>
-        <div>{}</div>
-        <div>{}</div>
-        <div>{}</div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
       </div>
       <div className="expense-item_description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </div >
   );
 };
 
