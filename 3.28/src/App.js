daba3ea8d2d471029a11aa269ecc42ab69883911
VWMAC <vincentkywong@gmail.com>
2021-07-10 23:56:56 -0700

    appJS returns expenses from compo not data

--- 3.28/src/App.js
+++ 3.28/src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
+
 
 function App() {
   const expenses = [
@@ -25,7 +26,7 @@ function App() {
 
   return (
     <div>
-      app.js
+      <Expenses />
     </div>
   );
 }
