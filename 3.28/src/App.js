0edecd41f976ab4ac41397c0df9a135721dfa773
VWMAC <vincentkywong@gmail.com>
2021-07-28 23:19:44 -0700

    alternate createElement syntax

--- 3.28/src/App.js
+++ 3.28/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Expenses from './components/Expenses';
 
 
@@ -24,12 +25,19 @@ function App() {
     },
   ];
 
-  return (
-    <div>
-      <h2>This is a spwa</h2>
-      <Expenses items={expenses} />
-    </div>
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h2', {}, 'This is a spwa'),
+    React.createElement(Expenses, { items: expenses })
   );
+
+  // return (
+  //   <div>
+  //     <h2>This is a spwa</h2>
+  //     <Expenses items={expenses} />
+  //   </div>
+  // );
 }
 
 export default App;
