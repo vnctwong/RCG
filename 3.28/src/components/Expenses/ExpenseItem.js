7016ed10684f3ea93ce2fc3a3ef356a136253382
VWMAC <vincentkywong@gmail.com>
2021-08-02 00:00:16 -0700

    console log clicked

--- 3.28/src/components/Expenses/ExpenseItem.js
+++ 3.28/src/components/Expenses/ExpenseItem.js
@@ -10,7 +10,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price"> ${props.amount} </div>
       </div>
-      <button onClick={() => { }}>Change title</button>
+      <button onClick={() => { console.log('clicked') }}>Change title</button>
     </Card >
   );
 };
