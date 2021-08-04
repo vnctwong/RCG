3cfd1b9de0f01040c96867d848ea62a477998528
VWMAC <vincentkywong@gmail.com>
2021-08-03 23:58:49 -0700

    bracket err

--- 3.28/src/components/Expenses/ExpenseItem.js
+++ 3.28/src/components/Expenses/ExpenseItem.js
@@ -6,8 +6,8 @@ const ExpenseItem = (props) => {
 
   const clickHandler = () => {
     console.log('Clicked');
-
   };
+
   return (
     <Card className="expense-item" >
       <ExpenseDate date={props.date} />
@@ -15,7 +15,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price"> ${props.amount} </div>
       </div>
-      <button onClick={clickHandler()}>Change title</button>
+      <button onClick={clickHandler}>Change title</button>
     </Card >
   );
 };
