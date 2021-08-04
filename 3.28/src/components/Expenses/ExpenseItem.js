5ba0cf1a667cd068802071a9313d5174e85c1b9d
VWMAC <vincentkywong@gmail.com>
2021-08-03 23:56:54 -0700

    add clickhandler

--- 3.28/src/components/Expenses/ExpenseItem.js
+++ 3.28/src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,11 @@ import './ExpenseItem.css';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+
+  const clickHandler = () => {
+    console.log('Clicked');
+
+  };
   return (
     <Card className="expense-item" >
       <ExpenseDate date={props.date} />
@@ -10,7 +15,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price"> ${props.amount} </div>
       </div>
-      <button onClick={() => { console.log('clicked') }}>Change title</button>
+      <button onClick={clickHandler()}>Change title</button>
     </Card >
   );
 };
